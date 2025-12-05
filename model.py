68e72029691f96fb3595583c0e4be4a5ad5f996a
solnypiotr <alt.pit111k@gmail.com>
2025-12-05 18:48:43 +0100

    created files and declarations of classes

---
+++ model.py
@@ -0,0 +1,17 @@
+class Model:
+    pass
+
+class GamState:
+    pass
+
+class CollisionManager:
+    pass
+
+class EntityManager:
+    pass
+
+class MenuState:
+    pass
+
+class Button:
+    pass
---
+++ view.py
@@ -0,0 +1,11 @@
+class View:
+    pass
+
+class MainView:
+    pass
+
+class MenuRenderer:
+    pass
+
+class GameRenderer:
+    pass
